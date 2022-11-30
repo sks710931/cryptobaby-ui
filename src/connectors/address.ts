9512613c64191b216dc74361dd5551c9b559a31d
Shivam Singh <shivamsingh071093@gmail.com>
2022-11-30 13:16:34 +0530

    smdjbhsv

--- src/connectors/address.ts
+++ src/connectors/address.ts
@@ -1,3 +1,3 @@
-export const NFTContract = "0x4E980F74bf07bf9481f00de2ce19FeC9c0aef274";
+export const NFTContract = "0x692EC62439C1d3B56f13B40B459c4257cB9b734e";
 export const rpc = "https://eth-goerli.g.alchemy.com/v2/MPqkYg1sKvXh_9WIhnGqeMI2gGABJ0tB";
 export const chainId = "0x5";
